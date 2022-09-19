74786b978ab48562f00d461b7f10597214938de2
Michelle Lawson <lawson_m@soshgic.edu.gh>
2022-09-19 20:18:37 +0000

    finished up and updated reflection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import a few useful containers from the typing module
 from calendar import c
 from typing import Dict, Union
+from computer import Computer
+from oo_resale_shop import ResaleShop
 
 # Import the functions we wrote in procedural_resale_shop.py
 from procedural_resale_shop import buy, update_price, sell, print_inventory, refurbish
@@ -19,61 +21,88 @@ def create_computer(description: str,
                     operating_system: str,
                     year_made: int,
                     price: int):
-    return {'description': description,
-            'processor_type': processor_type,
-            'hard_drive_capacity': hard_drive_capacity,
-            'memory': memory,
-            'operating_system': operating_system,
-            'year_made': year_made,
-            'price': price
+    return {Computer(description, processor_type, hard_drive_capacity, memory,operating_system, year_made, price)
     }
 
 def main():
     
-    # First, let's make a computer
-    computer = create_computer(
-        "Mac Pro (Late 2013)",
-        "3.5 GHc 6-Core Intel Xeon E5",
-        1024, 64,
-        "macOS Big Sur", 2013, 1500
+    store = ResaleShop({})
+
+    # First, we make a computer by calling an instance of our class
+    computer0 = Computer(
+        "HP 27.2 Delicious Pro",
+        "Intel 5.25 50 GHc 10-Core", 
+        1024, 128,
+        "Windows 11", 2012, 5200
     )
 
-    # Print a little banner
+    # user is able to add their own computer
+    '''
+    Create variable computer2
+    Take inpput from user for all parameters
+    validate parameter types
+    '''
+    print("You can add a computer now.")
+    computer1 = Computer(
+        str(input("Describe computer model: ")),
+        str(input("Describe processor type: ")),
+        int(input("What is the capacity of the Hard drive? ")),
+        int(input("How much memory can the computer store? ")),
+        str(input("What operating system is currently running on the computer? ")),
+        int(input("What year was this computer made? ")),
+        int(input("What is the price of this computer? "))
+    )
+
+    # Print a banner of the store name
     print("-" * 21)
     print("COMPUTER RESALE STORE")
     print("-" * 21)
 
-    # Add it to the resale store's inventory
-    print("Buying", computer["description"])
+    # Add two computers to the resale store's inventory
+    print("Buying", computer0.description)
     print("Adding to inventory...")
-    computer_id = buy(computer)
+    print("Buying", computer1.description)
+    print("Adding to inventory...")
+    computer_0_id = store.buy(computer0)
+    computer_1_id = store.buy(computer1)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    store.print_inventory()
     print("Done.\n")
 
     # Now, let's refurbish it
     new_OS = "MacOS Monterey"
-    print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
+    print("Refurbishing Item ID:", computer_0_id, ", updating OS to", new_OS)
     print("Updating inventory...")
-    refurbish(computer_id, new_OS)
+    store.refurbish(computer_0_id, new_OS)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    store.print_inventory()
     print("Done.\n")
     
     # Now, let's sell it!
-    print("Selling Item ID:", computer_id)
-    sell(computer_id)
+    print("Selling Item ID:", computer_0_id)
+    store.sell(computer_0_id)
     
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    store.print_inventory()
     print("Done.\n")
+
+    '''# Check to see if price can be updated
+    new_price = int(input("What is the new price for this computer?"))
+    print("Updating price...")
+    computer1.update_price(store, computer_1_id, new_price)
+    print("Done.\n")
+
+    # Make sure it worked by checking inventory
+    print("Checking inventory...")
+    store.print_inventory()
+    print("Done.\n")'''
 
 # Calls the main() function when this file is run
 if __name__ == "__main__": main()
